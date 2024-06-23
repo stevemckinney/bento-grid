8970d01016536dc824d400629a20d3a54ca6e5f6
Steve <341708+stevemckinney@users.noreply.github.com>
2024-06-23 12:16:31 +0100

    add an index page to get to options

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './option-1.html', './option-2.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
