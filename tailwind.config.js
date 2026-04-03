e2b19d3c1adbc8886d24fe5e99c0bee13a4261ec
Steve <341708+stevemckinney@users.noreply.github.com>
2026-04-03 20:11:12 +0100

    Move from tailwind alpha to stable

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./index.html', './option-1.html', './option-2.html'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
